4afc2a445c4d8c9d61a680569424d7fb248fb212
Dennis Mayk <dennis@mayk.eu>
2021-09-21 07:18:04 +0200

    update smooth_prob()

--- R/operate.R
+++ R/operate.R
@@ -108,7 +108,7 @@ flip_prob <- function(data, flip_dir=c('h','v')) {
     tibble::rowid_to_column("y")
   
   # Remove non-numerical from column names
-  colnames(data) <- c("y",paste(seq_len(ncol(data)-1)))
+  colnames(data) <- c("y",paste(seq_len(ncol(data))))
   
   # From wide to long format
   data <- pivot_longer(data, 
